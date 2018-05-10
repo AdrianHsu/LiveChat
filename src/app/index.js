d0395756ddf946bc95751671022abc4e877b9ba4
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-10 15:27:34 +0800

    try hot loader: start

--- src/app/index.js
+++ src/app/index.js
@@ -1,3 +1,4 @@
+'use strict';
 import React from 'react';
 import {render} from 'react-dom';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
+var BUILD_DIR = path.resolve(__dirname, 'src/public');
+var APP_DIR = path.resolve(__dirname, 'src/app');
 
 var config = {
   entry: APP_DIR + '/index.js',
