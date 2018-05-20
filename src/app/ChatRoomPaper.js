c913bf1a60b93ca052c719a71de4aa1532a264ed
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-20 22:43:22 +0800

    Room

---
+++ src/app/ChatRoomPaper.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ThemeProvider, MessageList, MessageGroup, Message,
+     MessageText } from '@livechat/ui-kit';
+
+
+class ChatRoomPaper extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    const friendname = this.props.friendname;
+    const friendicon = this.props.friendicon;
+
+    var msgItems = this.props.messageList.map( (item, i) => (
+    <Message avatarUrl={item.avatarUrl}
+      key={item.time + i}
+      date={item.time} isOwn={item.isOwn} authorName={item.authorName}>
+      <MessageText>{item.msg}</MessageText>
+    </Message>
+    ));
+    return (
+    <ThemeProvider>
+    <MessageList active style={{height: 570}}>
+        {msgItems}
+    </MessageList>
+    </ThemeProvider>)
+    
+  };  
+};
+
+export default ChatRoomPaper;
